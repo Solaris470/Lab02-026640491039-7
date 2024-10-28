56167795aa797eb467ae8838660233cc9380f3d2
Solaris470 <77920188+Solaris470@users.noreply.github.com>
2024-10-28 14:37:28 +0700

    กู้คื่น coming soon

--- app/routes/comingsoon.tsx
+++ app/routes/comingsoon.tsx
@@ -1,94 +1,14 @@
 import Navbar from "./template/header";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faBoxOpen, faChartLine, faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
-
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 export default function ComingSoon() {
-
-  const data = {
-    labels: ['0', '5k', '10k', '15k', '20k', '25k', '30k', '35k', '40k', '45k', '50k', '55k', '60k'],
-    datasets: [
-      {
-        label: 'Sales',
-        data: [30, 45, 28, 80, 99, 43, 76, 47, 45, 65, 41, 39, 47],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1,
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top' as const,
-      },
-    },
-    scales: {
-      y: {
-        beginAtZero: true,
-        min: 0,
-        max: 100,
-        ticks: {
-          stepSize: 20,
-          callback: (value: any) => `${value}%`,
-        },
-      },
-    },
-  };
-  
-
   return (
     <>
       <Navbar />
-      <div className="p-4 pt-16 sm:ml-64">
-        <h1 className="pt-6 pl-12 pb-2 text-start text-4xl font-medium">
-          Dashboard
-        </h1>
-        <div className="grid grid-cols-4 gap-6 mt-4 mb-8">
-          <div className="max-w-ml bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <p>Total User</p>
-              <h2 className="text-4xl text-end"><FontAwesomeIcon icon={faUsers} /></h2>
-            </div>
-            <h2 className="text-4xl font-bold mb-6">40200</h2>
-            <p>Up from yesterday</p>
-          </div>
-          <div className="max-w-ml bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <p>Total Order</p>
-              <h2 className="text-4xl text-end"><FontAwesomeIcon icon={faBoxOpen} /></h2>
-            </div>
-            <h2 className="text-4xl font-bold mb-6">40200</h2>
-            <p>Up from yesterday</p>
-          </div>
-          <div className="max-w-ml bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <p>Total Sales</p>
-              <h2 className="text-4xl text-end"><FontAwesomeIcon icon={faChartLine} /></h2>
-            </div>
-            <h2 className="text-4xl font-bold mb-6">40200</h2>
-            <p>Up from yesterday</p>
-          </div>
-          <div className="max-w-ml bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <p>Total Pending</p>
-              <h2 className="text-4xl text-end"><FontAwesomeIcon icon={faClockRotateLeft} /></h2>
-            </div>
-            <h2 className="text-4xl font-bold mb-6">40200</h2>
-            <p>Up from yesterday</p>
-          </div>
-        </div>
-
-        <div className="max-w-ml bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
-        <h2 className="text-3xl font-bold mb-6">Sales Details</h2>
-          <Line data={data} options={options} />
-        </div>
+      <div className="flex justify-center pt-64">
+      <img
+        src="https://media0.giphy.com/media/E0Ecgb6Dhve3v5eb9g/giphy.gif?cid=6c09b9520evom2dxqva6diba12smu54blu2t8s3pe74h212g&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s"
+        alt=""
+      />
       </div>
     </>
   );
--- app/routes/template/header.tsx
+++ app/routes/template/header.tsx
@@ -77,7 +77,7 @@ export default function Navbar() {
                   <ul className="py-1" role="none">
                     <li>
                       <a
-                        href="#"
+                        href="/dashboard"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"
                       >
@@ -128,7 +128,7 @@ export default function Navbar() {
           <ul className="space-y-2 font-medium">
             <li>
               <a
-                href="/comingsoon"
+                href="/dashboard"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
